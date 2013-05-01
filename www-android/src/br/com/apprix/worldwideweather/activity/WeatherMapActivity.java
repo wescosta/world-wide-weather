efc791ac335e8c5634876c1a9c7a1b210c84ce89
Wesley Lira <wesleylira@gmail.com>
2013-05-01 15:09:48 -0300

    Simplificação da Activity

--- www-android/src/br/com/apprix/worldwideweather/activity/WeatherMapActivity.java
+++ www-android/src/br/com/apprix/worldwideweather/activity/WeatherMapActivity.java
@@ -1,31 +1,41 @@
 package br.com.apprix.worldwideweather.activity;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import br.com.apprix.worldwideweather.R;
 
 public class WeatherMapActivity extends Activity {
 
-	@Override
-	@SuppressLint("SetJavaScriptEnabled")
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather_map);
-        
-        String url = getResources().getString(R.string.webapp_url);
-        
-        WebView webview = (WebView) findViewById(R.id.webview);
-        webview.getSettings().setJavaScriptEnabled(true);
-		webview.loadUrl(url);
-    }
+	private WebView webview = null;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_weather_map, menu);
-        return true;
-    }
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_weather_map);
+
+		initWebView();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_weather_map, menu);
+		return true;
+	}
+
+	private void initWebView() {
+		if (webview != null) return;
+
+		webview = (WebView) findViewById(R.id.webview);
+		
+		WebSettings settings = webview.getSettings();
+		settings.setJavaScriptEnabled(true);
+		settings.setGeolocationEnabled(true);
+		
+		String url = getResources().getString(R.string.webapp_url);
+		webview.loadUrl(url);
+	}
+
 }
